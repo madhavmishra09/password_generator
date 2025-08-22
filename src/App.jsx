0dfb4b715620a59481f0954056cdef190dad1ee2
unknown <madhavmishra083@gmail.com>
2025-08-22 16:32:15 +0530

    Password is now getting generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback,useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -17,9 +17,9 @@ function App() {
     }
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword])
-  useEffect(()=>{
+  useEffect(() => {
     passwordGenerator()
-  },[length, numberAllowed,charAllowed,passwordGenerator])
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-white bg-gray-800'>
@@ -30,15 +30,15 @@ function App() {
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
-            <input type="range" min={6} max={100} value={length} className='cursor-pointer' onChange={(e)=>{setLength(e.target.value)}}/>
+            <input type="range" min={6} max={100} value={length} className='cursor-pointer' onChange={(e) => { setLength(e.target.value) }} />
             <label>Length:{length}</label>
           </div>
           <div className='flex items-center gap-x-1'>
-            <input type="checkbox" defaultChecked={numberAllowed} id="numberInput" onChange={()=>{setNumberAllowed((prev)=> !prev)}} />
+            <input type="checkbox" defaultChecked={numberAllowed} id="numberInput" onChange={() => { setNumberAllowed((prev) => !prev) }} />
             <label htmlFor="numberInput">Numbers</label>
           </div>
           <div className='flex items-center gap-x-1'>
-            <input type="checkbox" defaultChecked={charAllowed} id="charInput" onChange={()=>{setCharAllowed((prev)=> !prev)}} />
+            <input type="checkbox" defaultChecked={charAllowed} id="charInput" onChange={() => { setCharAllowed((prev) => !prev) }} />
             <label htmlFor="charInput">Character</label>
           </div>
         </div>
